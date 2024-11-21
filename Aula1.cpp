d2aeabcc044702d649ec4761e556e18afe81bd06
jLucyJhon <jhonkirigaya2005@gmail.com>
2024-11-21 09:57:51 -0300

    commit
    O recomeço da linguagem

---
+++ Aula1.cpp
@@ -0,0 +1,10 @@
+// #include : serve para inserir bibliotecas
+#include <iostream> // biblioteca de entrada e saída
+
+using namespace std; // indica aonde estão as bibliotecas que serão usadas no código, organização de pastas.
+
+int main() // função booleana, caso ache o arquivo, retorna como true, se não achar, retorna como else.
+{
+
+    return 0;
+}
