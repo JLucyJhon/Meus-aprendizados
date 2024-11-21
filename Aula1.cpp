52a19c49be94391329c1c61348f76404270cc3f2
jLucyJhon <jhonkirigaya2005@gmail.com>
2024-11-21 11:05:22 -0300

    commit
    primeira parte

--- Aula1.cpp
+++ Aula1.cpp
@@ -5,6 +5,7 @@ using namespace std; // indica aonde estão as bibliotecas que serão usadas no 
 
 int main() // função booleana, caso ache o arquivo, retorna como true, se não achar, retorna como else.
 {
-
+    cout << "parabens, você conseguiu, agora vá embora" << endl; // saída do conteudo na tela, o endl serve para quebrar a linha durante o compilamento do código
+    system("pause"); // serve para realizar uma pausa no sistema enquanto ele é rodado, para que você possa teclar antes de parar.
     return 0;
 }
